50dea13493cecd15868efe4d5829f249740ac291
Guerinik Abderrahmane <ca.guerinik@esi-sba.dz>
2025-09-24 18:06:53 +0100

    add retreiver

--- rag_chatbot_esi_sba/vector_store.py
+++ rag_chatbot_esi_sba/vector_store.py
@@ -1,15 +1,20 @@
 import faiss
 from typing import List
 import numpy as np
+import os
+from openai import OpenAI
 
 
 class VectorStore:
     """ Store the vectors using faiss """
 
-    def __init__(self, d: int = 3072):
+    def __init__(self, d: int = 3072, model="text-embedding-3-large"):
+        openai_api_key = os.getenv("OPENAI_API_KEY")
         self.index = faiss.IndexFlatIP(d)
         self.metadata = []
         self.vectors = np.empty((0, d), dtype="float32")
+        self.client = OpenAI(api_key=openai_api_key)
+        self.model = model
 
     def add_embeddings(self, vectors: np.ndarray, metadata: List[dict]):
         """ 
@@ -21,17 +26,22 @@ class VectorStore:
         self.vectors = np.vstack([self.vectors, vectors])
         self.metadata.extend(metadata)
 
-    def search(self, query_vector, k=5):
+    def search(self, query, k=5):
         """ 
         search for the most k similar vectors
         Args:
-            query_vector: the embedding
+            query: the query
             k: number of similar vectors to return
         """
+        response = self.client.embeddings.create(
+            input=query,
+            model=self.model
+        )
+        query_vector = np.array([response.data[0].embedding], dtype="float32")
         D, I = self.index.search(query_vector, k)
         results_vecs = self.vectors[I]
         results_meta = [[self.metadata[j] for j in row] for row in I]
-        return results_vecs, results_meta
+        return I, results_meta, D
 
     def save_index(self, path="../data/vectorstore/index.faiss"):
         """ write the index on disk """
